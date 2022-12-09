975ccae5577022959cf5a3d2c7a6f0602b08774c
csik-patrik <csi.patrik455@gmail.com>
2022-12-09 11:31:10 +0100

    Implement nav bar

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,24 +1,39 @@
 <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Hidden brand</a>
+          <a class="navbar-brand" href="#">Moodle</a>
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">{{ __('Kezdőlap') }}<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
+              <a class="nav-link" href="#">link</a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                @guest
+                    <li class="nav-item active">
+                        <a class="btn btn-dark nav-link mr-1" href="{{ route('login') }}">{{ __('Bejelentkezés')}}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">{{ __('Regisztráció') }}</a>
+                    </li>    
+                @endguest
+                @auth
+                    <form method="POST" action="{{route('logout')}}">
+                        @csrf
+                        <button type="submit" class="btn btn-dark">Kijelentkezés</button>
+                    </form>  
+                @endauth
+                 
+            </ul>
           </form>
         </div>
       </nav>
