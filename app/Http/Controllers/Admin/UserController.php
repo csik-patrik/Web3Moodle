55fd767e9acf417cb0cd427be8612d6f6c20cd18
csik-patrik <csi.patrik455@gmail.com>
2022-12-10 10:57:00 +0100

    Add user controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,8 +4,94 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use App\http\Requests\Admin\UserStoreRequest;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('id', 'asc')->get();
+
+        return view('Admin.Users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::get();
+
+        return View('Admin.Users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserStoreRequest $request)
+    {
+        if ($request->validated()) {
+            $request['password'] = Hash::make($request['password']);
+
+            User::create($request->all());
+
+            return redirect()->route('admin.users.index', app()->getLocale())
+                        ->with('success', __('Felhasználó hozzáadása sikeres!'));
+        }
+        abort(403);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request)
+    {
+        $roles = Role::get();
+
+        $user = User::where('id', $request->user)->first();
+
+        return View('Admin.Users.edit', compact('roles', 'user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        User::where('id', $request->user)->delete();
+
+        return redirect()->route('admin.users.index', app()->getLocale())
+                        ->with('success', __('Felhasználó törlése sikeres!'));
+    }
 }
