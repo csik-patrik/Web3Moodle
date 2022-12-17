4841bfda5a15fd76000f837b3c1c1f417185524a
csik-patrik <csi.patrik455@gmail.com>
2022-12-17 17:38:25 +0100

    Add course seeder

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Course;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Course::create([
+            'code' => '8E1NKM6N',
+            'name' => 'Webprogramozás 3',
+            'category_id' => '1',
+            'owner_id' => '2'
+        ]);
+        Course::create([
+            'code' => '97MKNC01',
+            'name' => 'Magasszintű programozási nyelvek 2',
+            'category_id' => '1',
+            'owner_id' => '2'
+        ]);
+        Course::create([
+            'code' => 'W4ZQEHOK',
+            'name' => 'Robotika',
+            'category_id' => '1',
+            'owner_id' => '2'
+        ]);
+        Course::create([
+            'code' => '1FF8SFSC',
+            'name' => 'Algoritmusok 2',
+            'category_id' => '1',
+            'owner_id' => '2'
+        ]);
+    }
+}
