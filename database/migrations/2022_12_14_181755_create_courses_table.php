0148d146c26bb683ed3f642f0b74c5d5fc21645b
csik-patrik <csi.patrik455@gmail.com>
2022-12-17 16:18:33 +0100

    Add courses table

---
+++ database/migrations/2022_12_14_181755_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->bigIncrements('course_id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('owner_id');
+            //$table->foreign('category_id')->references('category_id')->on('course_categories')->onDelete('set null');
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
