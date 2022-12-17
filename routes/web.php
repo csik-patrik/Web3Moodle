6fd58e93636ca1b23590f036158b9bf291ff143f
csik-patrik <csi.patrik455@gmail.com>
2022-12-17 16:42:48 +0100

    Add courses resource route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Teacher\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('courses', CourseController::class);
 
 Route::group([
     'prefix' => 'admin',
