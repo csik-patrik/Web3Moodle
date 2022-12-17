6c027f202dc006686b1d8c22c92ba7ba196fd34b
csik-patrik <csi.patrik455@gmail.com>
2022-12-17 16:40:50 +0100

    Implement courses index method

--- app/Http/Controllers/Teacher/CourseController.php
+++ app/Http/Controllers/Teacher/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Teacher;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
@@ -14,7 +15,9 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        $courses = Course::orderBy('id', 'asc')->paginate(10);
+
+        return view('Teacher.Courses.index', compact('courses'));
     }
 
     /**
