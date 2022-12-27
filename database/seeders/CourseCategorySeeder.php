5d48b4a2aa6d49ecc185220e9c1a4489b560d7af
csik-patrik <csi.patrik455@gmail.com>
2022-12-27 15:11:04 +0100

    Add courseCategory seeder

---
+++ database/seeders/CourseCategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\CourseCategory;
+
+class CourseCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        CourseCategory::create([
+            'name' => 'Kötelező',
+        ]);
+
+        CourseCategory::create([
+            'name' => 'Kötelezően választott',
+        ]);
+
+        CourseCategory::create([
+            'name' => 'Szabadon választott',
+        ]);
+    }
+}
