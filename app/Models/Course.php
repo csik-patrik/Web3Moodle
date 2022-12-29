6b77a434aa89e17bb1cced2371cc9037be85fb29
csik-patrik <csi.patrik455@gmail.com>
2022-12-29 09:08:39 +0100

    Add log to models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Course extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $fillable = [
         'code',
@@ -24,5 +27,12 @@ class Course extends Model
     public function category()
     {
         return $this->belongsTo(CourseCategory::class, 'category_id');
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName}")
+        ->useLogName('Course');
     }
 }
